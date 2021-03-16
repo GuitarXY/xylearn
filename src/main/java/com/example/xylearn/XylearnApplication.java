7a95f26b017947bf6dd4332272c0b3fdd7113d2d
xiaoyao01 <xiaoyao01@changingedu.com>
2021-03-16 10:21:22 +0800

    first

---
+++ src/main/java/com/example/xylearn/XylearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.xylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XylearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XylearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xylearn/XylearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XylearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
